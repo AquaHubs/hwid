cf4d46ba57b34991117f6e14a9aa91c963e9ea75
AquaHub <kling10us@gmail.com>
2026-09-25 18:33:57 +0500

    Update ban.js

--- API/ban.js
+++ API/ban.js
@@ -3,12 +3,18 @@ import { Redis } from '@upstash/redis';
 const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
 
     const { hwid } = req.query;
 
     if (!hwid) {
-        return res.status(400).json({ error: 'HWID is required' });
+        return res.status(400).json({ error: 'HWID не указан' });
     }
 
     try {
